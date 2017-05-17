859ed406cbd1a842b122eec4d757d9639b0a15c9
J.Albert <jonathon.albert@live.com>
2017-05-17 23:14:06 +0000

    setup detail view for extra functionality
    add outlets

--- ImageDownloader/ImageGalleryDetailViewController.swift
+++ ImageDownloader/ImageGalleryDetailViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import MessageUI
 
 class ImageGalleryDetailViewController: UIViewController {
-
-    var imageGalleryObject : ImageGalleryObject?
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var objectImageView: UIImageView!
+    @IBOutlet weak var objectTitleLabel: UILabel!
+    @IBOutlet weak var objectTagsLabel: UILabel!
+    @IBOutlet weak var objectPublishedLabel: UILabel!
+    @IBOutlet weak var objectTakenLabel: UILabel!
+    
+    var imageGalleryObject : ImageGalleryObject? {
+        didSet {
+            setupInterface()
+        }
+    }
+    
+    private func setupInterface() {
+        view.setNeedsLayout()
+        
+        if let imageData = imageGalleryObject?.image {
+            objectImageView.image = UIImage(data:imageData)
+        }
+        
+        if let objectTitle = imageGalleryObject?.title {
+            objectTitleLabel.text = objectTitle
+        }
+        
+        if let objectTags = imageGalleryObject?.tags {
+            objectTagsLabel.text = objectTags
+        }
+        
+        if let objectDatePublished = imageGalleryObject?.datePublished {
+            objectPublishedLabel.text = String(describing: objectDatePublished)
+        }
+        
+        if let objectDateTaken = imageGalleryObject?.dateTaken {
+            objectTakenLabel.text = String(describing: objectDateTaken)
+        }
+    }
+    
+    
+    
         
     }
 }
